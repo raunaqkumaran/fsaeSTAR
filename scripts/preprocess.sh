3f9d1f98937175a542a49d24c490b4c9ed2354bf
raunaqkumaran <raunaq.kumaran@gmail.com>
2019-03-19 07:10:57 -0500

    Macro path fixes

--- scripts/preprocess.sh
+++ scripts/preprocess.sh
@@ -1,33 +1,3 @@
-<<<<<<< HEAD
-cd $PBS_O_WORKDIR;
-set -x
-
-export PODKEY="iOJDZCUKWsPIjut3CFrKXQ";
-export MACRO="macroController.java";
-export PROCESSES="20";
-export CP="/home/rkumaran/fsaeSTAR/src"
-export STARLOC="/home/rkumaran/Desktop/star/13.04.010-R8/STAR-CCM+13.04.010-R8/star/bin/starccm+"
-export SIMLOC="/scratch/scholar/rkumaran/Simulation_space/"
-export process="false"
-export preprocess="true"
-
-declare -a arr
-i=0
-
-for FILENAME in $SIMLOC*.sim; do
-  [ -e "$FILENAME" ] || continue
-  arr[$i]="$FILENAME"
-  i=$((i+1))
-done
-
-for ((j=0; j < $i; j++)); do
-  FILENAME=${arr[$j]}
-  $STARLOC -machinefile $PBS_NODEFILE -np $PROCESSES -power -podkey $PODKEY -batch $MACRO -classpath $CP $FILENAME
-  mv $FILENAME Preprocess
-done
-
-qdel $PBS_JOBID;
-=======
 cd $PBS_O_WORKDIR;
 set -x
 
@@ -56,4 +26,3 @@ for ((j=0; j < $i; j++)); do
 done
 
 qdel $PBS_JOBID;
->>>>>>> d239b7f... Fixed macro path
