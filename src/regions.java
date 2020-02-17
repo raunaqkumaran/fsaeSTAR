897204b74203346e66a8ed9a2d34abf3cea376fb
raunaqkumaran <raunaq.kumaran@gmail.com>
2020-02-17 01:12:30 -0500

    fixes for tyre rotation rate updates

--- src/regions.java
+++ src/regions.java
@@ -13,14 +13,6 @@ public class regions extends StarMacro {
 
         activeSim.activeSim.println("--- Setting up regions ---");
         double[] initialVector = simComponents.vectorRotate(simComponents.valEnv("Yaw"), simComponents.vectorScale(activeSim.freestreamVal * 0.1, activeSim.foreAftDirection));
-        double frontRotationRate = activeSim.freestreamVal / activeSim.frontTyreRadius;
-        double rearRotationRate = activeSim.freestreamVal / activeSim.rearTyreRadius;
-
-        if (activeSim.wtFlag) {
-            frontRotationRate = 0;
-            rearRotationRate = 0;
-        }
-
 
         activeSim.regionSwap();     // This is one of the few 'processes'
         // done within simComponents. Recreates the two main regions.
@@ -38,30 +30,7 @@ public class regions extends StarMacro {
 
         // Set up wheel boundary conditions
 
-        for (Boundary wheelBound : activeSim.wheelBounds) {
-            String boundName = wheelBound.getPresentationName();
-            wheelBound.getConditions().get(WallSlidingOption.class).
-                    setSelected(WallSlidingOption.Type.LOCAL_ROTATION_RATE);
-
-            try {
-
-                if (boundName.contains("Front")) {
-                    wheelBound.getValues().
-                            get(ReferenceFrame.class).setCoordinateSystem(activeSim.frontWheelCoord);
-                    wheelBound.getValues().get(WallRelativeRotationProfile.class).
-                            getMethod(ConstantScalarProfileMethod.class).getQuantity().
-                            setValue(frontRotationRate);
-                } else if (boundName.contains("Rear")) {
-                    wheelBound.getValues().get(ReferenceFrame.class).
-                            setCoordinateSystem(activeSim.rearWheelCoord);
-                    wheelBound.getValues().get(WallRelativeRotationProfile.class).
-                            getMethod(ConstantScalarProfileMethod.class).getQuantity().
-                            setValue(rearRotationRate);
-                }
-            } catch (Exception e) {
-                activeSim.activeSim.println("regions.java - Wheel boundary set-up failed");
-            }
-        }
+        setTyreRotation(activeSim);
 
         // Set up initial conditions
         activeSim.saPhysics.getInitialConditions().get(VelocityProfile.class).
@@ -98,6 +67,40 @@ public class regions extends StarMacro {
         }
     }
 
+    private void setTyreRotation(simComponents activeSim) {
+        for (Boundary wheelBound : activeSim.wheelBounds) {
+            double frontRotationRate = activeSim.freestreamVal / activeSim.frontTyreRadius;
+            double rearRotationRate = activeSim.freestreamVal / activeSim.rearTyreRadius;
+
+            if (activeSim.wtFlag) {
+                frontRotationRate = 0;
+                rearRotationRate = 0;
+            }
+            String boundName = wheelBound.getPresentationName();
+            wheelBound.getConditions().get(WallSlidingOption.class).
+                    setSelected(WallSlidingOption.Type.LOCAL_ROTATION_RATE);
+
+            try {
+
+                if (boundName.contains("Front")) {
+                    wheelBound.getValues().
+                            get(ReferenceFrame.class).setCoordinateSystem(activeSim.frontWheelCoord);
+                    wheelBound.getValues().get(WallRelativeRotationProfile.class).
+                            getMethod(ConstantScalarProfileMethod.class).getQuantity().
+                            setValue(frontRotationRate);
+                } else if (boundName.contains("Rear")) {
+                    wheelBound.getValues().get(ReferenceFrame.class).
+                            setCoordinateSystem(activeSim.rearWheelCoord);
+                    wheelBound.getValues().get(WallRelativeRotationProfile.class).
+                            getMethod(ConstantScalarProfileMethod.class).getQuantity().
+                            setValue(rearRotationRate);
+                }
+            } catch (Exception e) {
+                activeSim.activeSim.println("regions.java - Wheel boundary set-up failed");
+            }
+        }
+    }
+
     private void setDomainBoundaries(simComponents activeSim) {
             activeSim.leftPlane.setBoundaryType(SymmetryBoundary.class);
             activeSim.symPlane.setBoundaryType(SymmetryBoundary.class);
@@ -118,6 +121,7 @@ public class regions extends StarMacro {
 
     public void yawInterfaces(simComponents activeSim) {
         setDomainBoundaries(activeSim);
+        setTyreRotation(activeSim);
 
         if (activeSim.fullCarFlag) {
             activeSim.activeSim.println("Setting boundary conditions for yaw");
