e9b2797834c73ba3098938fd8e627c3cdd109819
Arpit Agarwal <32472124+Arpit2206@users.noreply.github.com>
2020-05-20 00:02:09 -0400

    Create movefiles.bat
    Batch File to automatically move files from one server to another

---
+++ Docs/movefiles.bat
@@ -0,0 +1,3 @@
+rem This Batch Script moves CFD files from ME FSAE Drive to Scholar Scratch Folder
+
+Robocopy "Z:\2021\7_Aerodynamics\7_CFD" "S:\jpeerbol\Simulation_space\Sim_Uploads\Goons" /S /MOV /R:n /W:n
