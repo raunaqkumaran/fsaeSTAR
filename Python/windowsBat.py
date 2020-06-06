e59bb4226d88ab65261a63cdfbc23ace46b3fcee
raunaqkumaran <raunaq.kumaran@gmail.com>
2020-06-06 17:02:35 -0400

    Update windowsBat.py

--- Python/windowsBat.py
+++ Python/windowsBat.py
@@ -1,18 +1,19 @@
-'''This is literally the definition of a one-off piece of code. Just writing this so i can make the 5 terabytes of
-sim files I have less massive. '''
+'''Applies a macro on a set of files in a given folder'''
 
 import batchBuilderSupport as bbs
 import os
 import sys
+
 path = r"D:\Archive\Formula\2020"
 sizeLimit = 400e6
 fileList = bbs.get_file_list(path, sizeLimit)
 starPath = r'"C:\Program Files\Siemens\15.02.009-R8\STAR-CCM+15.02.009-R8\star\bin\starccm+.exe"'
 macroPath = r"C:\Users\rauna\Documents\GitHub\fsaeSTAR\src\universalMeshClear.java"
+classPath = r"C:\Users\rauna\Documents\GitHub\fsaeSTAR\src"
 outputFile = "deleteEverything.bat"
 output = open(outputFile, "w")
 for x in fileList:
-    cmdString = starPath + " -power -podkey " + "5SaJLFgZg7E9iD6YITAjlw" + " -batch " + macroPath + ' "' + x + '"'
+    cmdString = starPath + " -power -podkey " + "5SaJLFgZg7E9iD6YITAjlw" + " -classpath " + classPath + " -batch " + macroPath + ' "' + x + '"'
     output.write(cmdString)
     output.write(os.linesep)
 output.close()
