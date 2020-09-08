a9faad5656c796e740c15c82c1d1b214fa116cd0
raunaqkumaran <raunaq.kumaran@gmail.com>
2020-09-07 23:40:44 -0400

    Update windowsBat.py

--- Python/windowsBat.py
+++ Python/windowsBat.py
@@ -4,11 +4,11 @@ import batchBuilderSupport as bbs
 import os
 import sys
 
-path = r"D:\PostProc"
+path = r"D:\2021 Simulations"
 sizeLimit = 400e6
 fileList = bbs.get_file_list(path, sizeLimit)
 starPath = r'"C:\Program Files\Siemens\15.02.009-R8\STAR-CCM+15.02.009-R8\star\bin\starccm+.exe"'
-macroPath = [r"C:\Users\rauna\Documents\GitHub\fsaeSTAR\src\exportReports.java", r"C:\Users\rauna\Documents\GitHub\fsaeSTAR\src\postProc.java"]
+macroPath = [r"C:\Users\rauna\Documents\GitHub\fsaeSTAR\src\universalMeshClear.java"]
 classPath = r"C:\Users\rauna\Documents\GitHub\fsaeSTAR\src"
 outputFile = "deleteEverything.bat"
 output = open(outputFile, "w")
