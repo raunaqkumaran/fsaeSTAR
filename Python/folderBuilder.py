c63eaf333b41857f30bbeb99a0a16b040801d8b4
cjloeb <59874560+cjloeb@users.noreply.github.com>
2020-10-23 18:18:21 -0400

    Update folderBuilder.py

--- Python/folderBuilder.py
+++ Python/folderBuilder.py
@@ -2,56 +2,62 @@ import batchBuilderSupport as bbs
 import os
 
 # INPUTS HERE
-
-configFileName = "linuxConfig.config"
+importPath = os.getcwd() # current working directory call; change to wherever the .configs are exported to
 
 # FUNCTION CALLS
 
-# Get config vars
-configFile = open(configFileName, "r")
-controllerVars = bbs.get_env_vals(configFile)
+# Iterates through every file in importPath. This pulls all files with ".config" extension, so don't put unwanted ".config" files in here
+for contents in os.listdir(importPath):
+    configFileName = contents
+    if (os.path.splitext(configFileName)[1] == ".config"):
 
-if controllerVars['CLUSTER'] != "LOCAL":
-    if int(controllerVars['PROCS']) < 20:
-        controllerVars['NODES'] = 1
-    else:
-        controllerVars['NODES'] = int(int(controllerVars['PROCS']) / 20)
+        # Get config vars
+        configFile = open(importPath + configFileName, "r")
 
-    controllerVars['PPN'] = int(int(controllerVars['PROCS']) / controllerVars['NODES'])
+        controllerVars = bbs.get_env_vals(configFile)
 
-path = controllerVars['SIMPATH']
+        if controllerVars['CLUSTER'] != "LOCAL":
+            if int(controllerVars['PROCS']) < 20:
+                controllerVars['NODES'] = 1
+            else:
+                controllerVars['NODES'] = int(int(controllerVars['PROCS']) / 20)
 
-# Get command strings
-posixCommand = bbs.generatecommand(controllerVars)
-qsubCommand = bbs.generateqsub(controllerVars)
+            controllerVars['PPN'] = int(int(controllerVars['PROCS']) / controllerVars['NODES'])
 
-# Get file list
-file_list = bbs.get_file_list(path)
+        path = controllerVars['SIMPATH']
 
-# Set up output files
+        # Get command strings
+        posixCommand = bbs.generatecommand(controllerVars)
+        qsubCommand = bbs.generateqsub(controllerVars)
 
-timeStamp = bbs.get_timestamp()
-outputFileName = timeStamp + "_" + "controller.sh"
-outputFile = open(outputFileName, "wb")
-outputFile.write("#!/bin/sh\n".encode())
-for key, value in controllerVars.items():
-    bbs.posix_write_flag(key, value, outputFile)
+        # Get file list
+        file_list = bbs.get_file_list(path)
 
-if controllerVars['CLUMPED'] == "true":
-    child_scripts = bbs.clumped(file_list, configFile, posixCommand)
-    outputFile.write((qsubCommand + '"' + child_scripts + '"').encode())
-    bbs.posix_write_blanks(outputFile, 2)
-    os.system("chmod +x " + '"' + child_scripts + '"')
+        # Set up output files
 
-else:
-    child_scripts = bbs.individuals(file_list, configFile, posixCommand)
-    for x in child_scripts:
-        outputFile.write((qsubCommand + '"' + x + '"').encode())
-        bbs.posix_write_blanks(outputFile, 2)
-        os.system("chmod +x " + '"' + x + '"')
+        timeStamp = bbs.get_timestamp()
+        outputFileName = timeStamp + "_" + "controller.sh"
+        outputFile = open(outputFileName, "wb")
+        outputFile.write("#!/bin/sh\n".encode())
+        for key, value in controllerVars.items():
+            bbs.posix_write_flag(key, value, outputFile)
 
-outputFile.close()
+        if controllerVars['CLUMPED'] == "true":
+            child_scripts = bbs.clumped(file_list, configFile, posixCommand)
+            outputFile.write((qsubCommand + '"' + child_scripts + '"').encode())
+            bbs.posix_write_blanks(outputFile, 2)
+            os.system("chmod +x " + '"' + child_scripts + '"')
+
+        else:
+            child_scripts = bbs.individuals(file_list, configFile, posixCommand)
+            for x in child_scripts:
+                outputFile.write((qsubCommand + '"' + x + '"').encode())
+                bbs.posix_write_blanks(outputFile, 2)
+                os.system("chmod +x " + '"' + x + '"')
+
+        outputFile.close()
+        configFile.close()
 
 
-os.system("chmod +x " + outputFileName)
-os.system("./" + outputFileName)
+        os.system("chmod +x " + outputFileName)
+        os.system("./" + outputFileName)
