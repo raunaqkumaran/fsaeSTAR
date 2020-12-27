6d69865b7c8a3ed6de2b8a92ef49f142e14a840c
raunaqkumaran <raunaq.kumaran@gmail.com>
2020-12-26 20:15:47 -0500

    added a utility to remove old slurm files and shell scripts.

---
+++ Python/clearFolder.py
@@ -0,0 +1,12 @@
+import os
+
+current_dir = os.getcwd()
+
+all_files = [current_dir + os.sep + x for x in os.listdir(current_dir)]
+
+for file in all_files:
+    if file.endswith(".sh") or file.endswith(".out") or file.startswith("nodefile"):
+        print("Removing: " + file)
+        os.remove(file)
+
+print("----Complete----")
