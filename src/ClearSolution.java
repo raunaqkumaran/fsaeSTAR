50c3d398c4a01ae5c6e155e3fefe8dfcb9563c0c
raunaqkumaran <raunaq.kumaran@gmail.com>
2020-12-29 14:47:31 -0500

    Create ClearSolution.java

---
+++ src/ClearSolution.java
@@ -0,0 +1,9 @@
+import star.common.StarMacro;
+
+public class ClearSolution extends StarMacro {
+    @Override
+    public void execute() {
+        SimComponents activeSim = new SimComponents(getActiveSimulation());
+        activeSim.clearSoln();
+    }
+}
