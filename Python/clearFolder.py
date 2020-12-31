cd368032145e440cdc43d8574fcb94482194d6ae
raunaqkumaran <raunaq.kumaran@gmail.com>
2020-12-30 19:14:57 -0500

    Update clearFolder.py

--- Python/clearFolder.py
+++ Python/clearFolder.py
@@ -5,7 +5,7 @@ current_dir = os.getcwd()
 all_files = [current_dir + os.sep + x for x in os.listdir(current_dir)]
 
 for file in all_files:
-    if file.endswith(".sh") or file.endswith(".out") or file.contains("nodefile."):
+    if file.endswith(".sh") or file.endswith(".out") or "nodefile." in file:
         print("Removing: " + file)
         os.remove(file)
 
