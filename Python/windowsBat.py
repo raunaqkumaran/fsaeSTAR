a698a21d2275c8e875bbb007b548268229463b8c
raunaqkumaran <raunaq.kumaran@gmail.com>
2021-03-20 02:10:34 -0400

    Update windowsBat.py

--- Python/windowsBat.py
+++ Python/windowsBat.py
@@ -4,7 +4,7 @@ import batchBuilderSupport as bbs
 import os
 import sys
 
-path = r"D:\OneDrive\Formula SAE\CFD\Working sim"
+path = r"D:\Archive\Formula"
 sizeLimit = 400e6
 fileList = bbs.get_file_list(path, sizeLimit)
 starPath = r'"C:\Program Files\Siemens\15.06.008-R8\STAR-CCM+15.06.008-R8\star\bin\starccm+.exe"'
@@ -13,6 +13,7 @@ classPath = r"C:\Users\rauna\Documents\GitHub\fsaeSTAR\src"
 outputFile = "deleteEverything.bat"
 output = open(outputFile, "w")
 for x in fileList:
+    print("Writing line for ", x)
     for y in macroPath:
         cmdString = starPath + " -power -podkey " + "ejp6DLYl3MCoXIiuatSpAg" + " -classpath " + bbs.add_quotes(classPath) + " -batch " + bbs.add_quotes(y) + " " + bbs.add_quotes(x)
         output.write(cmdString)
